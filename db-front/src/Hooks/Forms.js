853547541975d12e7a635a9ab3452350c26abd22
miguch <vvchan@outlook.com>
2021-11-21 15:20:26 -0500

    chore: update nginx config

--- db-front/src/Hooks/Forms.js
+++ db-front/src/Hooks/Forms.js
@@ -2,12 +2,12 @@ import { Form } from 'antd';
 
 export default function useForm(formSchema) {
   const [form] = Form.useForm();
-  
+
   return (
       <Form>
         {formSchema.map(item => (
-          <Form.Item>
-
+          <Form.Item label={item.title} name={item.name} rules={item.rules}>
+            
           </Form.Item>
         ))}
       </Form>
