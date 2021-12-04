4909659e46527e0fdd93e1dd81d1f56b51c8c26e
miguch <vvchan@outlook.com>
2021-12-04 10:22:55 -0500

    fix: fix issue with cascade delete of product kind

--- db_backend/api/product-kind/controllers/product-kind.js
+++ db_backend/api/product-kind/controllers/product-kind.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+    await strapi.connections.default.transaction(async (trx) => {
+      // use foreign key constraint
+      entity = await strapi
+        .query("product-kind")
+        .findOne({ id }, { transacting: trx });
+      await strapi.connections.default
+        .raw(`DELETE FROM \`product_kinds\` WHERE id=?`, [id])
+        .transacting(trx);
+    });
+
+    return entity;
+  },
+};
